3c5455aa113a78b9d2fae006885cbe1975fdda72
frtasoniero <frtasoniero@gmail.com>
2024-07-21 02:13:23 -0300

    Add interfaces

--- noteProject/main.go
+++ noteProject/main.go
@@ -7,10 +7,28 @@ import (
 	"strings"
 
 	"github.com/frtasoniero/golang/note/note"
+	"github.com/frtasoniero/golang/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
 
 func main() {
 	title, content := getNoteData()
+	todoText := getTodoDate()
+
+	userTodo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +37,39 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(userTodo)
 
 	if err != nil {
-		fmt.Println("Saving the note failed!")
 		return
 	}
 
+	err = outputData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed!")
+		return err
+	}
+
 	fmt.Println("Saving the note succeeded!")
+	return nil
+}
+
+func getTodoDate() string {
+	text := getUserInput("Todo text: ")
+	return text
 }
 
 func getNoteData() (string, string) {
